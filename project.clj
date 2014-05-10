8f579ca1ce5f8cef036f0a9ff77e1d30e2799739
Dom Kiva-Meyer <git@domkm.com>
2014-05-10 15:46:09 -0700

    update optimus version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [optimus "0.14.2"]
+                 [optimus "0.14.3"]
                  [org.jruby/jruby-complete "1.7.11"]
                  [zweikopf "1.0.0" :exclusions [org.jruby/jruby-complete]]]
   :resource-paths ["gems"])
