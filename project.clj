fe17ebd8ec2908cb09e08e69db0f4ba2021445f1
Dom Kiva-Meyer <git@domkm.com>
2014-05-10 14:31:32 -0700

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject optimus-sass "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject optimus-sass "0.0.1"
+  :description "Sass/SCSS asset loader for Optimus"
+  :url "http://github.com/DomKM/optimus-sass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [optimus "0.14.2"]
+                 [org.jruby/jruby-complete "1.7.11"]
+                 [zweikopf "1.0.0" :exclusions [org.jruby/jruby-complete]]]
+  :resource-paths ["gems"])
