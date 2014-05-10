d26a1f3a6e5218ecb069a12ee3d03c68d0bfe4bc
Dom Kiva-Meyer <git@domkm.com>
2014-05-10 14:47:31 -0700

    provide sass/scss asset loader for optimus

--- src/optimus_sass/core.clj
+++ src/optimus_sass/core.clj
@@ -1,6 +1,31 @@
-(ns optimus-sass.core)
+(ns optimus-sass.core
+  (:require [clojure.string :as str]
+            [optimus.assets.creation :refer [last-modified existing-resource]]
+            [optimus.assets.load-css :refer [create-css-asset]]
+            [zweikopf.multi :refer [call-ruby ruby-eval]])
+  (:import [org.jruby.embed ScriptingContainer LocalContextScope]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:private ruby
+  (ScriptingContainer. LocalContextScope/SINGLETON))
+
+(ruby-eval ruby "ENV['GEM_PATH']='gems'
+                 ENV['GEM_HOME']='gems'
+                 require 'sass'")
+
+(def ^:private sass (ruby-eval ruby "Sass"))
+
+(defn ^:private compile-sass [path]
+  (call-ruby ruby sass :compile_file path))
+
+(defn ^:private load-sass-asset [public-dir path]
+  (let [resource (existing-resource public-dir path)
+        css (-> resource .getFile compile-sass)
+        css-asset (create-css-asset (str/replace path #"\.sass\z|\.scss\z" ".css")
+                                    css
+                                    (last-modified resource))]
+    (assoc css-asset :original-path path)))
+
+(doseq [ext ["sass" "scss"]]
+  (defmethod optimus.assets.creation/load-asset ext
+    [public-dir path]
+    (load-sass-asset public-dir path)))
