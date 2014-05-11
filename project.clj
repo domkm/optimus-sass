f71a301672f5bceed663f9e27fd57724e3832f41
Dom Kiva-Meyer <git@domkm.com>
2014-05-10 17:27:36 -0700

    remove zweikopf dependency

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [optimus "0.14.3"]
                  [org.jruby/jruby-complete "1.7.11"]
-                 [zweikopf "1.0.0" :exclusions [org.jruby/jruby-complete]]]
+                 [optimus "0.14.3"]]
   :resource-paths ["deps"]
   :global-vars {*warn-on-reflection* true})
--- src/optimus_sass/core.clj
+++ src/optimus_sass/core.clj
@@ -1,23 +1,18 @@
 (ns optimus-sass.core
   (:require [clojure.string :as str]
             [optimus.assets.creation :refer [last-modified existing-resource]]
-            [optimus.assets.load-css :refer [create-css-asset]]
-            [zweikopf.multi :refer [call-ruby ruby-eval]])
+            [optimus.assets.load-css :refer [create-css-asset]])
   (:import [org.jruby.embed ScriptingContainer LocalContextScope]))
 
-(def ^:private ruby
-  (ScriptingContainer. LocalContextScope/SINGLETON))
+(let [ruby (ScriptingContainer. LocalContextScope/SINGLETON)
+      sass (.runScriptlet ruby "ENV['GEM_PATH']='deps'
+                                ENV['GEM_HOME']='deps'
+                                require 'sass'
+                                Sass")]
+  (defn- compile-sass [^String path]
+    (.callMethod ruby sass "compile_file" path String)))
 
-(ruby-eval ruby "ENV['GEM_PATH']='deps'
-                 ENV['GEM_HOME']='deps'
-                 require 'sass'")
-
-(def ^:private sass (ruby-eval ruby "Sass"))
-
-(defn ^:private compile-sass [path]
-  (call-ruby ruby sass :compile_file path))
-
-(defn ^:private load-sass-asset [public-dir path]
+(defn- load-sass-asset [public-dir path]
   (let [resource (existing-resource public-dir path)
         css (-> resource .getFile compile-sass)
         css-asset (create-css-asset (str/replace path #"\.sass\z|\.scss\z" ".css")
