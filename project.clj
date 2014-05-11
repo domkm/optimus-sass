5518a0badc80fdf61d87da2fbb09e0bc96492a56
Dom Kiva-Meyer <git@domkm.com>
2014-05-11 15:14:34 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject optimus-sass "0.0.1"
+(defproject optimus-sass "0.0.2"
   :description "Sass/SCSS asset loader for Optimus"
   :url "http://github.com/DomKM/optimus-sass"
   :license {:name "Eclipse Public License"
