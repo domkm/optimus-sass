aa6552596b799f1a0888b12370b22f0139db428e
Dom Kiva-Meyer <git@domkm.com>
2014-12-21 20:37:28 -0800

    make `compile-file` function public

--- src/optimus_sass/core.clj
+++ src/optimus_sass/core.clj
@@ -1,5 +1,6 @@
 (ns optimus-sass.core
-  (:require [clojure.string :as str]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [optimus.assets.creation :refer [last-modified existing-resource]]
             [optimus.assets.load-css :refer [create-css-asset]])
   (:import [org.jruby.embed ScriptingContainer LocalContextScope]))
@@ -9,12 +10,12 @@
                                 ENV['GEM_HOME']='deps'
                                 require 'sass'
                                 Sass")]
-  (defn- compile-sass [^String path]
-    (.callMethod ruby sass "compile_file" path String)))
+  (defn compile-file [^java.io.File file]
+    (.callMethod ruby sass "compile_file" (.getPath file) String)))
 
 (defn- load-sass-asset [public-dir path]
   (let [resource (existing-resource public-dir path)
-        css (-> resource .getFile compile-sass)
+        css (-> resource io/file compile-file)
         css-asset (create-css-asset (str/replace path #"\.sass\z|\.scss\z" ".css")
                                     css
                                     (last-modified resource))]
