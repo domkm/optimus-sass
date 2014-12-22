85ec4fe60e8213e33b013dd94e80cf3f5433a191
Dom Kiva-Meyer <git@domkm.com>
2014-12-21 20:36:23 -0800

    update optimus dep

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.jruby/jruby-complete "1.7.11"]
-                 [optimus "0.14.3"]]
+                 [optimus "0.15.1"]]
   :resource-paths ["deps"]
   :global-vars {*warn-on-reflection* true})
