41412b47565e163107c4ac6fbc6a9382b3e0cfc9
Abby Henríquez Tejera <paradoja@gmail.com>
2020-01-26 19:32:31 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/DomKM/optimus-sass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.jruby/jruby-complete "1.7.11"]
-                 [optimus "0.15.1"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.jruby/jruby-complete "9.2.9.0"]
+                 [optimus "0.20.2"]]
   :resource-paths ["deps"]
   :global-vars {*warn-on-reflection* true})
